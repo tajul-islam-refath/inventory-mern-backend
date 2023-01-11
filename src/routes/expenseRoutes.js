d4512ccb5512ae14f716b6ada67fb6ba59625c48
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-01-11 08:09:07 +0600

    fect: Added expense module. Into expense  module I include routes,controller and model.

---
+++ src/routes/expenseRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const AuthVerifyMiddleware = require("../middlewares/AuthVerifyMiddleware");
+
+const ExpensesController = require("../controllers/Expenses/ExpensesController");
+
+router.post(
+  "/CreateExpenses",
+  AuthVerifyMiddleware,
+  ExpensesController.CreateExpenses
+);
+router.post(
+  "/UpdateExpenses/:id",
+  AuthVerifyMiddleware,
+  ExpensesController.UpdateExpenses
+);
+router.get(
+  "/ExpensesList/:pageNo/:perPage/:searchKeyword",
+  AuthVerifyMiddleware,
+  ExpensesController.ExpensesList
+);
+
+router.get(
+  "/ExpenseDetailsByID/:id",
+  AuthVerifyMiddleware,
+  ExpensesController.ExpenseDetailsByID
+);
+
+module.exports = router;
