51616cf853bb4c2eef4d74f746abfc011d3b6ed9
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-01-11 08:35:08 +0600

    fact: Add product module. product module deal with product. create a ListTwoJoinService, this methode join tow model and return

---
+++ src/routes/productRoute.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const AuthVerifyMiddleware = require("../middlewares/AuthVerifyMiddleware");
+
+const ProductsController = require("../controllers/Products/ProductsController");
+
+router.post(
+  "/CreateProducts",
+  AuthVerifyMiddleware,
+  ProductsController.CreateProducts
+);
+router.post(
+  "/UpdateProducts/:id",
+  AuthVerifyMiddleware,
+  ProductsController.UpdateProducts
+);
+router.get(
+  "/ProductsList/:pageNo/:perPage/:searchKeyword",
+  AuthVerifyMiddleware,
+  ProductsController.ProductsList
+);
+// router.get("/DeleteProduct/:id",AuthVerifyMiddleware,ProductsController.DeleteProduct);
+router.get(
+  "/ProductsDetailsByID/:id",
+  AuthVerifyMiddleware,
+  ProductsController.ProductsDetailsByID
+);
+router.get(
+  "/ProductsDropDown",
+  AuthVerifyMiddleware,
+  ProductsController.ProductsDropDown
+);
+
+module.exports = router;
