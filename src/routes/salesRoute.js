7c2fe1c2e69f040fd4f5436f55151bf556e7fdad
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-01-17 21:03:58 +0600

    fect: Add Sales model,controller and routes

---
+++ src/routes/salesRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const AuthVerifyMiddleware = require("../middlewares/AuthVerifyMiddleware");
+const SalesController = require("../controllers/Sales/SalesController");
+const router = express.Router();
+
+router.post("/CreateSales", AuthVerifyMiddleware, SalesController.CreateSales);
+router.get(
+  "/SalesList/:pageNo/:perPage/:searchKeyword",
+  AuthVerifyMiddleware,
+  SalesController.SalesList
+);
+// router.get("/SaleDelete/:id",AuthVerifyMiddleware,SalesController.SaleDelete);
+
+module.exports = router;
