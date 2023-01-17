b6b761fb471259b928b1262863672fb1a085b2ca
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-01-17 09:02:58 +0600

    fect: Add purchases model,controller, routes. Implement database transation concept for create purchases and purchases product

---
+++ src/routes/purchaseRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const PurchasesController = require("../controllers/Purchases/PurchasesController");
+const AuthVerifyMiddleware = require("../middlewares/AuthVerifyMiddleware");
+
+router.post(
+  "/CreatePurchases",
+  AuthVerifyMiddleware,
+  PurchasesController.CreatePurchases
+);
+router.get(
+  "/PurchasesList/:pageNo/:perPage/:searchKeyword",
+  AuthVerifyMiddleware,
+  PurchasesController.PurchasesList
+);
+// router.get("/PurchasesDelete/:id",AuthVerifyMiddleware,PurchasesController.PurchasesDelete);
+
+module.exports = router;
